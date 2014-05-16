d689d77fe0d5f84a2cc22038c108fe990550e8c7
Peter Strøiman <peter@stroiman.com>
2014-05-16 11:53:39 +0200

    Created naive Banking example

---
+++ Banking.Specs/AccountSpec.fs
@@ -0,0 +1,35 @@
+﻿module AccountSpec
+open Banking
+open FSpec.Core
+open Dsl
+open MatchersV3
+open TestContextOperations
+
+let specs =
+  describe "Account" [
+    subject (fun _ -> new Account())
+
+    context "when withdrawing cash" [
+      context "account is in balance" [
+        before (fun ctx -> 
+          let account = ctx |> getSubject<Account>
+          account.Balance <- 500)
+
+        it "account dispenses cash" (fun ctx ->
+          let account = ctx |> getSubject<Account>
+          account.CanWithdraw(60) |> should (be.equalTo true)
+        )
+      ]
+          
+      context "account is overdrawn" [
+        before (fun ctx -> 
+          let account = ctx |> getSubject<Account>
+          account.Balance <- -500)
+
+        it "the Account does not dispense cash" (fun ctx ->
+          let account = ctx |> getSubject<Account>
+          account.CanWithdraw(60) |> should (be.equalTo false)
+        )
+      ]
+    ]
+  ]
---
+++ Banking/Account.cs
@@ -0,0 +1,12 @@
+﻿namespace Banking
+{
+    public class Account
+    {
+        public int Balance { get; set; }
+
+        public bool CanWithdraw(int amount)
+        {
+            return amount < Balance;
+        }
+    }
+}
